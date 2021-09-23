861c05fdacff8dd5be3a2b8ed801a469a662ffcb
markus <jh>
2021-09-23 13:37:37 +0200

    mandelbrot GPU

--- mandelbrotGPU/main.py
+++ mandelbrotGPU/main.py
@@ -1,9 +1,6 @@
-import numpy as np
-import time
 from model import Model
 from view import View
 from controller import Controller
-from utils import *
 
 
 if __name__ == "__main__":
@@ -13,3 +10,11 @@ if __name__ == "__main__":
     controller = Controller(model, view)
 
     controller.run()
+
+"""
+Important resource that got me started with numba CUDA:
+https://notebook.community/arasdar/DL/impl-dl/etc/misc3/numba-cuda-gpu-example
+
+Making a single image from this tutorial was simply adapted to fit into my
+MVC pattern template.
+"""
